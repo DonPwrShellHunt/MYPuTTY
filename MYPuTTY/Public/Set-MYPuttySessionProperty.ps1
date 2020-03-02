8d63ee35097c819f47ba8f91d8bafa7af203f26c
Don Hunt <DonPwrShellHunt@users.noreply.github.com>
2020-03-01 19:29:27 -0500

    Fix Begin/Process blocks to allow session array

--- MYPuTTY/Public/Set-MYPuttySessionProperty.ps1
+++ MYPuTTY/Public/Set-MYPuttySessionProperty.ps1
@@ -19,7 +19,7 @@ function Set-MYPuttySessionProperty {
             [Parameter(Mandatory=$true,
                        ValueFromPipelineByPropertyName=$true,
                        Position=0)]
-            [String]
+            [String[]]
             $Name,
 
             # PropertyHash contains the names & values of the properties to be set
@@ -31,41 +31,49 @@ function Set-MYPuttySessionProperty {
 
         Begin {
             $puttypath = 'HKCU:\Software\SimonTatham\PuTTY\Sessions'
-            $subkeypath = Join-Path -Path $puttypath -ChildPath $Name
-
-            $allprops = @(Get-PuttySessionPropertySet -Name $Name)
-            if ($allprops.Count -gt 1) {
-                # create hash where property name is key, type is value (String, Dword)
-                $pntype = @{}
-                foreach ($p in $allprops) {
-                    $pntype[$p.PropertyName] = $p.PropertyType
-                }
-            } else {
-                Write-Error -Message ('Invalid Session Name: {0}' -f $Name)
-                throw 'Bad session name'
-            }
         }
 
         Process {
-            foreach ($k in $PropertyHash.keys) {
-
-                switch ($pntype[$k])
-                {
-                    'String'  {
-                        [String]$value = $PropertyHash[$k]
-                        if ($PSCmdlet.ShouldProcess($k)) {
-                            Set-ItemProperty -Path $subkeypath -Name $k -Value $value -Verbose
-                        }
+            foreach ($sessionName in $Name) {
+                $subkeypath = Join-Path -Path $puttypath -ChildPath $sessionName
+                # get all the properties for this session, for validation of incoming hash & registry data type
+                $allprops = @(Get-MYPuttySessionPropertySet -Name $sessionName)
+                if ($allprops.Count -gt 1) {
+                    # create hash where property name is key, type is value (String, Dword)
+                    $pntype = @{}
+                    foreach ($p in $allprops) {
+                        $pntype[$p.PropertyName] = $p.PropertyType
                     }
+                } else {
+                    Write-Error -Message ('Invalid Session Name: {0}' -f $sessionName)
+                    throw 'Bad session name'
+                }
 
-                    'Dword'   {
-                        if ($PSCmdlet.ShouldProcess($k)) {
-                            Set-ItemProperty -Path $subkeypath -Name $k -Value $PropertyHash[$k] -Verbose
+                foreach ($k in $PropertyHash.keys) {
+                    # would be nice to check each key (property name) is valid
+                    # in current logic this could be done by testing existance in $pntype hash
+                    if ( $pntype.ContainsKey($k) ) {
+                        switch ($pntype[$k])
+                        {
+                            'String'  {
+                                [String]$value = $PropertyHash[$k]
+                                if ($PSCmdlet.ShouldProcess($k)) {
+                                    Set-ItemProperty -Path $subkeypath -Name $k -Value $value -Verbose
+                                }
+                            }
+
+                            'Dword'   {
+                                if ($PSCmdlet.ShouldProcess($k)) {
+                                    Set-ItemProperty -Path $subkeypath -Name $k -Value $PropertyHash[$k] -Verbose
+                                }
+                            }
+
+                            default   {
+                                Write-Warning -Message ('For PropertyName: {0} Unhandled Type: {1}' -f $k,$pntype[$k])
+                            }
                         }
-                    }
-
-                    default   {
-                        Write-Warning -Message ('For PropertyName: {0} Unhandled Type: {1}' -f $k,$pntype[$k])
+                    } else {
+                        Write-Warning -Message ('PropertyName: {0} does not exist in Session: {1}' -f $pntype[$k],$sessionName)
                     }
                 }
             }
